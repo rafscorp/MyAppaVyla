5b5792e7af80c989e5e28a4bfc8bfbebe9fd6b9a
rafscorp <metsaccout@gmail.com>
2026-02-23 12:05:01 -0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -8,37 +8,11 @@ export default defineConfig({
   root: 'www',
   publicDir: 'public',
   base: './',
-  plugins: [
-    {
-      name: 'remove-cordova-script',
-      enforce: 'pre',
-      transformIndexHtml(html) {
-        // Remove a tag cordova.js antes do Vite processar para evitar erro de bundle
-        return html.replace(/<script[^>]*src=["']cordova\.js["'][^>]*>[\s\S]*?<\/script>/gi, '');
-      }
-    },
-    {
-      name: 'inject-cordova-script',
-      enforce: 'post',
-      transformIndexHtml(html) {
-        // Reinsere a tag cordova.js no final do body para o build final
-        return html.replace('</body>', '    <script src="cordova.js"></script>\n</body>');
-      }
-    },
-    {
-      name: 'expose-gsap-global',
-      transform(code, id) {
-        // Intercepta o bootstrap.js para injetar o GSAP globalmente
-        if (id.endsWith('bootstrap.js')) {
-          return `import { gsap } from 'gsap';\nwindow.gsap = gsap;\n${code}`;
-        }
-      }
-    }
-  ],
+  plugins: [],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    chunkSizeWarningLimit: 4000, // Aumenta o limite para 3MB (silencia o aviso do vendor.bundle.js)
+    chunkSizeWarningLimit: 4000,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'www/index.html'),
@@ -49,7 +23,6 @@ export default defineConfig({
       },
       output: {
         entryFileNames: (chunkInfo) => {
-            // Mantém o Service Worker na raiz para escopo correto
             return chunkInfo.name === 'sw' ? '[name].js' : 'js/[name].js';
         },
         chunkFileNames: 'js/[name].js',
@@ -57,7 +30,6 @@ export default defineConfig({
           if (assetInfo.name && assetInfo.name.endsWith('.css')) {
             return 'css/[name][extname]';
           }
-          // Remove hash para facilitar cache manual no SW
           return 'assets/[name][extname]';
         },
         manualChunks: (id) => {
